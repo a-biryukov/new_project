b6f069a96877306b01a6810a5f7b1f9fba28da37
Alexander <a.biryukov164@gmail.com>
2024-01-18 22:04:43 +0400

    new function capitalized_words

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def string_in_uppercase(string) -> str:
+    """
+    Возвращает сроку в верхнем регистре
+    """
     return string.upper()
+
+
+def capitalized_words(string) -> str:
+    """
+    Возвращает строку в которой каждое слово начинается с заглавной буквы
+    """
+    return string.title()()
